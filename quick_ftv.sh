2f6bf443af82ebb24bec8af55d5742896aaf73fc
Raphael Vorias <raphaelvorias@hannes-air.home>
2021-10-30 14:19:50 +0200

    Add pipeline v2

--- quick_ftv.sh
+++ quick_ftv.sh
@@ -1,3 +1,5 @@
 #/bin/sh
 
-wine FileToVox-v1.13-win/FileToVox.exe --i output/height_13337.png -o output/map --hm=75 -c --cm output/color_13337.png
+wine FileToVox-v1.13-win/FileToVox.exe --i output/height_13337.png -o output/map --hm=75 --cm output/color_13337.png
+
+#wine FileToVox-v1.13-win/FileToVox.exe --i .vox --o .vox --s ./flood.json
